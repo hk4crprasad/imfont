d1a0efda9f5067a0facbeea517e1e96a0a01d304
PIRODEVELEPOR <82955298+hk4crprasad@users.noreply.github.com>
2023-11-28 17:28:37 +0530

    Update main.py 0.0.4

--- imfont/main.py
+++ imfont/main.py
@@ -1,6 +1,7 @@
 import os
 import shutil
 from imfont import point
+import sys
 
 def copy_and_chmod(src, dest):
     try:
@@ -8,28 +9,33 @@ def copy_and_chmod(src, dest):
         os.chmod(dest, 0o777)
         return True
     except Exception as e:
-        print(f"\033[1;31mError: {e}.\033[0m")
+        print(f"\033[1;31mError during copy_and_chmod: {e}.\033[0m")
         return False
-
+prefix_path = sys.prefix
 def main():
     try:
         font2c_path = f"{prefix_path}/bin/font2c"
         if os.path.exists(font2c_path):
             point()
         else:
-            if "termux" in os.environ.get("SHELL", "").lower():
+            if os.path.exists("/data/data/com.termux/files/usr/bin"):
+                # For Termux
                 termux_path = f"{prefix_path}/lib/python3.11/site-packages/imfont/font2c"
                 if copy_and_chmod(termux_path, font2c_path):
                     point()
                 else:
                     print("\033[1;31mFailed to copy and chmod font2c for Termux.\033[0m")
             else:
-                if copy_and_chmod("/usr/local/lib/python3.9/dist-packages/imfont/font2c", "/usr/bin/font2c"):
+                # For other Linux systems
+                other_linux_path = "/usr/local/lib/python3.9/dist-packages/imfont/font2c"
+                if copy_and_chmod(other_linux_path, "/usr/bin/font2c"):
                     point()
                 else:
-                    print("\033[1;31mFailed to copy and chmod font2c for other Linux distributions.\033[0m")
-    except Exception as e:
-        print("\033[1;31mAn unexpected error occurred.\033[0m")
+                    print("\033[1;31mFailed to copy and chmod font2c for other Linux systems.\033[0m")
+    except Exception as main_error:
+        print(f"\033[1;31mAn unexpected error occurred in main(): {main_error}.\033[0m")
+        import traceback
+        traceback.print_exc()
 
 if __name__ == "__main__":
     main()
