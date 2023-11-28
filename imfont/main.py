54118c9c450f1926d7410f70d2f7c8d07efbdd5a
PIRODEVELEPOR <82955298+hk4crprasad@users.noreply.github.com>
2023-11-28 17:03:21 +0530

    Update main.py

--- imfont/main.py
+++ imfont/main.py
@@ -6,7 +6,12 @@ def main():
         if os.path.exists(f"{prefix_path}/bin/font2c"):
             point()
         else:
-            os.system(f"cp {prefix_path}/lib/python3.11/site-packages/imfont/font2c {prefix_path}/bin/font2c")
+            try:
+                os.system(f"cp {prefix_path}/lib/python3.*/site-packages/imfont/font2c {prefix_path}/bin/font2c")
+                os.system(f"chmod 777 {prefix_path}/bin/font2c")
+            except Exception as e:
+                os.system(f"cp /usr/local/lib/python3.*/dist-packages/imfont/font2c /usr/bin/font2c")
+                os.system(f"chmod 777 /usr/bin/font2c")
             point()
     except Exception as e:
         print(f"\033[1;31mError: {e}.\033[0m")
